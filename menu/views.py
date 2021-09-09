ed6e4ab1ff0753a9974bf712251e208f2c2cb5d7
NicolaLampis <nico.lampo@gmail.com>
2021-09-09 17:59:15 +0000

    added selector that sort by price and name

--- menu/views.py
+++ menu/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import (
 from .models import Product, Category, Deal
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 
 
 def all_products(request):
@@ -11,8 +12,24 @@ def all_products(request):
     query = None
     categories = None
     deals = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'favourite':
+                sortkey = 'favourite'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -33,10 +50,13 @@ def all_products(request):
             queries = Q(name__icontains=query) | \
                 Q(description__icontains=query)
             products = products.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
     context = {
         'products': products,
         'search_term': query,
         'current_category': categories,
         'deals': deals,
+        'current_sorting': current_sorting,
     }
     return render(request, 'menu/menu.html', context)
