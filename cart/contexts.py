d251d954c3c816a11dd760b6b6e20f2c2ca48f53
NicolaLampis <nico.lampo@gmail.com>
2021-09-11 15:08:22 +0000

    improve contexts, added shopping cart page

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,27 +1,41 @@
 from django.conf import settings
+from menu.models import Product
+from django.shortcuts import get_object_or_404
+from decimal import Decimal
 
 
 def cart_contents(request):
     """ Makes the cart content available across the entire site """
     cart_items = []
-    sub_total = 0
+    total = 0
     order_total = 0
     product_count = 0
     delivery_threshold = settings.FREE_DELIVERY_THRESHOLD
     delivery_charge = settings.DELIVERY_CHARGE
+    cart = request.session.get('cart', {})
 
-    if sub_total < delivery_threshold:
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
+    if total < delivery_threshold:
         delivery = delivery_charge
-        free_delivery_delta = delivery_threshold - sub_total
+        free_delivery_delta = Decimal(delivery_threshold) - Decimal(total)
     else:
         delivery = 0
         free_delivery_delta = 0
 
-    order_total = delivery + sub_total
+    order_total = Decimal(delivery) + Decimal(total)
 
     context = {
         'cart_items': cart_items,
-        'sub_total': sub_total,
+        'total': total,
         'product_count': product_count,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
