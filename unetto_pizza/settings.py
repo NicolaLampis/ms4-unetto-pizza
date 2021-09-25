1096fa3488e8822027ba6f51f9165d0124b8dc88
NicolaLampis <nico.lampo@gmail.com>
2021-09-25 12:15:14 +0000

    deployment heroku, part 2

--- unetto_pizza/settings.py
+++ unetto_pizza/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-d1-p2p%jk^tpep4fc*++a)$6()&lxl6qe-e*@*yo=*5ph-&5$-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['unetto-pizza.herokuapp,com', 'localhost']
 
 
 # Application definition
@@ -117,12 +117,17 @@ WSGI_APPLICATION = 'unetto_pizza.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:    
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
