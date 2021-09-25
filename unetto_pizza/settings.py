2bb33784fff082166629c8280f1df7083a00e2dd
NicolaLampis <nico.lampo@gmail.com>
2021-09-25 13:13:54 +0000

    Removed secret key and set debug

--- unetto_pizza/settings.py
+++ unetto_pizza/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d1-p2p%jk^tpep4fc*++a)$6()&lxl6qe-e*@*yo=*5ph-&5$-'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['unetto-pizza.herokuapp.com', 'localhost']
 
